12740f649def6c4bb94cdfe8baa8f88d9fa7317b
Yulia Fil <filyulia.yf@gmail.com>
2025-10-06 00:12:25 +0300

    fix: fixed constants

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -4,8 +4,8 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const DATA_FILE = path.join(__dirname, "../data/bills.json");
-export const KEYWORDS_FILE = path.join(__dirname, "../data/keywords.json");
+export const DATA_FILE = path.join(__dirname, "../data/storedBills.json");
+export const KEYWORDS_FILE = path.join(__dirname, "../data/keyWords.json");
 
 export const PORT = process.env.PORT || 3000;
 export const URL = "https://itd.rada.gov.ua/billinfo/Bills/period";
