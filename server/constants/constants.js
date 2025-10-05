6fd9df9fa24bc496b991564ac860089884b73b1f
Yulia Fil <filyulia.yf@gmail.com>
2025-10-06 00:05:44 +0300

    refactor: changed constants

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -1,6 +1,13 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const DATA_FILE = path.join(__dirname, "../data/bills.json");
+export const KEYWORDS_FILE = path.join(__dirname, "../data/keywords.json");
+
 export const PORT = process.env.PORT || 3000;
 export const URL = "https://itd.rada.gov.ua/billinfo/Bills/period";
-export const KEYWORDS_FILE = "server/data/keyWords.json";
-export const DATA_FILE = "server/data/storedBills.json";
 
 export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
