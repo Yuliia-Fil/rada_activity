1ea9f11e5716fd1e4af345c239d0381111006c82
Yulia Fil <filyulia.yf@gmail.com>
2025-10-06 00:35:20 +0300

    fix: fixed scrapeAllBills

--- server/services/scrapeAllBills.js
+++ server/services/scrapeAllBills.js
@@ -7,18 +7,9 @@ export async function scrapeAllBills() {
   try {
     browser = await puppeteer.launch({
       headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-accelerated-2d-canvas",
-        "--no-first-run",
-        "--no-zygote",
-        "--single-process",
-        "--disable-gpu",
-      ],
+      executablePath: "/usr/bin/chromium-browser",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
-
     const page = await browser.newPage();
     await page.goto(URL, { waitUntil: "networkidle2" });
 
